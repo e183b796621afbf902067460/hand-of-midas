cdbe1c2f2a10a8753c1bd8c4256b2a14fb9c20d6
Semyon Puchin <spuchin@samokat.ru>
2025-06-03 15:40:28 +0300

    Added APIClientBase

---
+++ src/adapters/clients/common/api_base.py
@@ -0,0 +1,26 @@
+from abc import ABC
+from typing import Final
+
+from attr import attrs
+from httpx import URL, AsyncClient, Response
+
+_GET: Final[str] = "GET"
+_POST: Final[str] = "POST"
+
+
+@attrs(slots=True, auto_attribs=True, kw_only=True)
+class APIClientBase(ABC):
+
+    _session: AsyncClient
+
+    async def __request(self, method: str, endpoint: str, parameters: dict | None) -> Response | None:
+        url: URL = self._session.base_url.join(url=endpoint)
+        response: Response | None = None
+        if method == _GET:
+            response = await self._session.get(url=url, params=parameters)
+        elif method == _POST:
+            response = await self._session.post(url=url, params=parameters)
+        return response
+
+    async def _get(self, endpoint: str, parameters: dict | None) -> Response | None:
+        return await self.__request(method=_GET, endpoint=endpoint, parameters=parameters)
