30768b546918d6a79c782b545fb551db941099de
Semyon Puchin <spuchin@samokat.ru>
2025-06-12 15:19:02 +0300

    Added CandlesticksRepository

--- src/adapters/repositories/common/clickhouse_base.py
+++ src/adapters/repositories/common/clickhouse_base.py
@@ -1,9 +1,16 @@
+from datetime import datetime
+from typing import Final
+
 from attr import attrs
 from clickhouse_connect import get_async_client
 from clickhouse_connect.driver.asyncclient import AsyncClient
+from clickhouse_connect.driver.query import QueryResult
 from pandas import DataFrame
 
 from src.settings import settings
+
+_CLICKHOUSE_UNIX_EPOCH_YEAR: Final[int] = 1970
+_CLICKHOUSE_UNIX_EPOCH: Final[datetime] = datetime(year=_CLICKHOUSE_UNIX_EPOCH_YEAR, month=1, day=1, hour=0, minute=0)
 
 
 async def get_clickhouse_client() -> AsyncClient:
@@ -12,7 +19,20 @@ async def get_clickhouse_client() -> AsyncClient:
 
 @attrs(slots=True, auto_attribs=True, kw_only=True)
 class ClickHouseBaseRepository:
+
     _client: AsyncClient
 
-    async def _query(self, query: str, parameters: tuple | None = None) -> DataFrame:
+    @staticmethod
+    def _unix_epoch_to_none(timestamp: datetime) -> datetime | None:
+        if timestamp == _CLICKHOUSE_UNIX_EPOCH:
+            return None
+        return timestamp
+
+    async def _query(self, query: str, parameters: dict | None = None) -> QueryResult:
+        return await self._client.query(query=query, parameters=parameters)
+
+    async def _query_dataframe(self, query: str, parameters: dict | None = None) -> DataFrame:
         return await self._client.query_df(query=query, parameters=parameters)
+
+    async def _insert_dataframe(self, table: str, dataframe: DataFrame) -> None:
+        await self._client.insert_df(table=table, df=dataframe)
