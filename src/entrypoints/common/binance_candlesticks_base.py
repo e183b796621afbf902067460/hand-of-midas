6d856c440eb682822e5b290bf687822d21993461
Semyon Puchin <606d18446a06fe9738fd@gmail.com>
2025-06-12 22:34:42 +0500

    Added Binance SPOT 4h candlesticks and Binance USDT-M 4h candlesticks DAGs (#11)

---
+++ src/entrypoints/common/binance_candlesticks_base.py
@@ -0,0 +1,61 @@
+from datetime import datetime
+
+from clickhouse_connect.driver.asyncclient import AsyncClient as AsyncClickHouseClient
+from httpx import AsyncClient as HTTPAsyncClient
+from httpx import AsyncHTTPTransport
+from pandas import DataFrame
+
+from src.adapters.clients.binance import BinanceSpotAPIClient, BinanceUsdtmAPIClient
+from src.adapters.repositories.candlesticks import CandlesticksRepository
+from src.adapters.repositories.common.clickhouse_base import get_clickhouse_client
+from src.schemas.binance import BinanceIntervalEnum, BinanceKlinesInputSchema, BinanceSectionEnum
+from src.schemas.candlesticks import CandlesticksLatestTimestampInputSchema
+from src.services.binance import BinanceService
+from src.services.candlesticks import CandlesticksService
+from src.settings import settings
+
+
+# pylint: disable=too-many-locals
+async def main(ticker: str, exchange: BinanceSectionEnum, interval: BinanceIntervalEnum) -> None:
+    binance_api_session: HTTPAsyncClient = HTTPAsyncClient(
+        base_url=settings.BINANCE_SPOT_API_HTTP_URL.unicode_string()
+        if exchange is BinanceSectionEnum.BINANCE_SPOT
+        else settings.BINANCE_USDTM_API_HTTP_URL.unicode_string(),
+        timeout=settings.BINANCE_SPOT_API_TIMEOUT
+        if exchange is BinanceSectionEnum.BINANCE_SPOT
+        else settings.BINANCE_USDTM_API_TIMEOUT,
+        transport=AsyncHTTPTransport(
+            retries=settings.BINANCE_SPOT_API_RETRIES
+            if exchange is BinanceSectionEnum.BINANCE_SPOT
+            else settings.BINANCE_USDTM_API_RETRIES,
+            http2=True,
+        ),
+    )
+    binance_api_client: BinanceSpotAPIClient | BinanceUsdtmAPIClient = (
+        BinanceSpotAPIClient(session=binance_api_session)
+        if exchange is BinanceSectionEnum.BINANCE_SPOT
+        else BinanceUsdtmAPIClient(session=binance_api_session)
+    )
+    await binance_api_client.ping()
+    clickhouse_client: AsyncClickHouseClient = await get_clickhouse_client()
+    candlesticks_repository: CandlesticksRepository = CandlesticksRepository(client=clickhouse_client)
+    candlesticks_service: CandlesticksService = CandlesticksService(repository=candlesticks_repository)
+    binance_service: BinanceService = BinanceService(client=binance_api_client)
+
+    latest_timestamp: datetime = await candlesticks_service.get_latest_timestamp(
+        input_schema=CandlesticksLatestTimestampInputSchema(ticker=ticker, exchange=exchange, interval=interval)
+    )
+    candlesticks: DataFrame = await binance_service.get_klines(
+        input_schema=BinanceKlinesInputSchema(
+            symbol=ticker,
+            section=exchange,
+            interval=interval,
+            start_time=latest_timestamp,
+            end_time=settings.TRIGGER_DATE,
+        )
+    )
+    candlesticks.drop_duplicates(inplace=True)
+    await candlesticks_service.paste_candlesticks(dataframe=candlesticks)
+
+
+# pylint: enable=too-many-locals
